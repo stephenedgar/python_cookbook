7d37d60eb5b05965ea2f1859178772bcde9d0eca
Stephen Edgar <stephen@1partcarbon.co.uk>
2020-09-02 13:17:12 +0100

    user input

---
+++ user_input.py
@@ -0,0 +1,6 @@
+# print("Hello, please enter your name")
+# name = input("What is your name? ")
+# print("Hello " + name)
+
+number = input("What is your favourite number? ")
+print(int(number) * 3)
